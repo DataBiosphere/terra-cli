456a1b22b7b3cf7214e2de45e99cb313db4246e9
wchambers <1499870+wchamber@users.noreply.github.com>
2021-02-05 16:58:06 -0500

    Add delete command.

--- src/main/java/bio/terra/cli/command/Notebook.java
+++ src/main/java/bio/terra/cli/command/Notebook.java
@@ -1,6 +1,7 @@
 package bio.terra.cli.command;
 
 import bio.terra.cli.command.notebook.Create;
+import bio.terra.cli.command.notebook.Delete;
 import picocli.CommandLine;
 
 /**
@@ -10,5 +11,5 @@ import picocli.CommandLine;
 @CommandLine.Command(
     name = "notebook",
     description = "Commands related to AI Notebooks in the Terra workspace context.",
-    subcommands = {Create.class})
+    subcommands = {Create.class, Delete.class})
 public class Notebook {}
