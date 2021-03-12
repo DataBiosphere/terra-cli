90002d4fa453fda9328905e6698dc19b29f0a883
Mariko Medlock <mariko.medlock@gmail.com>
2021-03-12 12:20:03 -0500

    gh release create.

---
+++ tools/publish-release.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+## This script builds a new GitHub release for the Terra CLI.
+## Usage: ./publish-release.sh        --> installs the latest version
+
+# TODO: build and publish the Docker image
+
+echo "-- Building the distribution archive"
+./gradlew distTar
+
+echo "-- Creating a new GitHub release with the install archive and download script"
+#gh release create --draft "${GITHUB_REF#refs/tags/}" dist/*.tar tools/download-install.sh
+gh release create --draft "$RELEASE_VERSION" dist/*.tar tools/download-install.sh
