99ed0df8da3ede5f4aed7da581b040f29f39b6b0
Mariko Medlock <mariko.medlock@gmail.com>
2021-03-12 12:40:05 -0500

    Include source code archive.

--- tools/publish-release.sh
+++ tools/publish-release.sh
@@ -8,6 +8,11 @@
 echo "-- Building the distribution archive"
 ./gradlew distTar
 
+echo "-- Building the source code archive"
+SOURCE_CODE_ARCHIVE_FILENAME="terra-cli-$RELEASE_VERSION.tar.gz"
+tar -czvf $SOURCE_CODE_ARCHIVE_FILENAME ./
+
 echo "-- Creating a new GitHub release with the install archive and download script"
 #gh release create --draft "${GITHUB_REF#refs/tags/}" dist/*.tar tools/download-install.sh
-gh release create --draft "$RELEASE_VERSION" build/distributions/*.tar tools/download-install.sh
+gh release create --draft "$RELEASE_VERSION" \
+  build/distributions/*.tar tools/download-install.sh $SOURCE_CODE_ARCHIVE_FILENAME
