434127590f896949cb44dc86496874a315f42289
Mariko Medlock <mariko.medlock@gmail.com>
2021-03-12 15:45:32 -0500

    Remove draft release.

--- tools/publish-release.sh
+++ tools/publish-release.sh
@@ -4,12 +4,10 @@
 ## Inputs: RELEASE_VERSION (env var, required) determines the git tag to use for creating the release
 ## Usage: ./publish-release.sh
 
-echo "-- Building the source code archive"
-sourceCodeArchiveFilename="terra-cli-${RELEASE_VERSION}.tar.gz"
-sourceCodeArchivePath=../$sourceCodeArchiveFilename
-tar -czf $sourceCodeArchivePath ./
-echo "sourceCodeArchivePath: $sourceCodeArchivePath"
-ls -la ..
+#echo "-- Building the source code archive"
+#sourceCodeArchiveFilename="terra-cli-${RELEASE_VERSION}.tar.gz"
+#sourceCodeArchivePath=../$sourceCodeArchiveFilename
+#tar -czf $sourceCodeArchivePath ./
 
 # TODO: include Docker image build and publish, once Vault secrets have been added to the GH repo
 #echo "-- Building the Docker image"
@@ -21,13 +19,12 @@ echo "-- Building the distribution archive"
 ./gradlew distTar
 distributionArchivePath=$(ls build/distributions/*tar)
 echo "distributionArchivePath: $distributionArchivePath"
-#mariko
 
 echo "-- Creating a new GitHub release with the install archive and download script"
 #gh release create --draft "${GITHUB_REF#refs/tags/}" dist/*.tar tools/download-install.sh
 releaseTag="v${RELEASE_VERSION}"
 gh release create $releaseTag \
-  --draft \
+#  --draft \
   --title "v${RELEASE_VERSION}" \
   "${distributionArchivePath}#Install package" \
   "tools/download-install.sh#Download & Install script" \
