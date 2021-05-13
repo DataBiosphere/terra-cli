080db59b3c0d3455231653ecf95e79e4ed0ee36c
wchambers <1499870+wchamber@users.noreply.github.com>
2021-05-13 16:00:08 -0400

    PF-763 Initial post startup script

---
+++ notebooks/README.md
@@ -0,0 +1,5 @@
+# AI Notebooks Scripts
+
+Directory for AI Notebook scripts.
+
+For now, we're experimenting with post startup scripts for customizing Terra AI Notebook instances. While these scripts may evventually migrate to a notebook specific repository, for now its convenient to have them here.
---
+++ notebooks/post-startup.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# Default post startup script for AI notebooks
+
+# TODO git config?
+# TODO source Terra workspace id as env variable from metadata server?
+
+# Install these globally (not in a virtual environment)\n",
+pip3 install --upgrade pre-commit nbdime nbstripout pylint pytest
+
+# Install nbstripout globally
+nbstripout --install
+
+# Install & configure the Terra CLI
+
+# TODO should we just use the latest version?
+mkdir ~/bin
+cd ~/bin || exit
+env TERRA_CLI_VERSION="0.38.0" curl -L https://github.com/DataBiosphere/terra-cli/releases/latest/download/download-install.sh | bash
+echo 'export PATH=${HOME}/bin:${PATH}' >> ~/.bash_profile
+./terra
+
+# Set browser manual since that is always what we want for cloudtops.
+./terra config set browser manual
+
+# TODO mount the current workspace?
