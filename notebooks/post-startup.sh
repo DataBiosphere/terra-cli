8aab0d81504f9f6c6bec6242276aeff844ce25f2
wchambers <1499870+wchamber@users.noreply.github.com>
2021-05-14 16:16:40 -0400

    tweaks, cli working

--- notebooks/post-startup.sh
+++ notebooks/post-startup.sh
@@ -8,7 +8,6 @@ exec 2>&1
 set -x
 
 echo "USER: ${USER}"
-echo "whoami ${whoami}"
 
 # The linux user that JupyterLab will be running as. It's important to do most installation in the
 # user space.
@@ -21,6 +20,7 @@ echo "/etc/passwd"
 cat /etc/passwd
 
 # Install these globally (not in a virtual environment)\n",
+sudo apt-get install python-setuptools
 sudo -u ${VM_USER} sh -c 'pip3 install --user --upgrade pre-commit nbdime nbstripout pylint pytest'
 
 # Install nbstripout globally
@@ -31,12 +31,10 @@ sudo -u ${VM_USER} sh -c 'nbstripout --install'
 # TODO should we just use the latest version?
 cd /tmp || exit
 sudo -u ${VM_USER} sh -c 'env TERRA_CLI_VERSION="0.38.0" curl -L https://github.com/DataBiosphere/terra-cli/releases/latest/download/download-install.sh | bash'
-sudo -u ${VM_USER} sh - c './terra'
 
 # Set browser manual since that is always what we want for cloudtops.
 sudo -u ${VM_USER} sh -c './terra config set browser manual'
 
 sudo cp terra /usr/bin/terra
-echo "copied"
 
 # TODO mount the current workspace?
