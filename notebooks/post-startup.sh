96f7eaf0355aa9887eae1b7a49691139528bd9a1
wchambers <1499870+wchamber@users.noreply.github.com>
2021-05-14 14:41:50 -0400

    tweaks

--- notebooks/post-startup.sh
+++ notebooks/post-startup.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
+# Default post startup script for AI notebooks
 
 # Send the output from this script to a tmp file.
 exec >> /tmp/post-startup-output.txt
 exec 2>&1
 
-# The Linux user the notebook will be running as
-NOTEBOOK_USER="jupyter"
-
-# Switch to the 'jupyter' user used in AI notebooks.
-cd /home/jupyter || echo "cd /home/${NOTEBOOK_USER} failed" && exit
-
-# Default post startup script for AI notebooks
+echo "USER: ${USER}"
 
 # TODO git config?
 # TODO source Terra workspace id as env variable from metadata server?
+
+echo "/etc/passwd"
+cat /etc/passwd
 
 # Install these globally (not in a virtual environment)\n",
 pip3 install --upgrade pre-commit nbdime nbstripout pylint pytest
@@ -24,13 +22,14 @@ nbstripout --install
 # Install & configure the Terra CLI
 
 # TODO should we just use the latest version?
-mkdir bin
-cd bin || exit
+cd /tmp || exit
 env TERRA_CLI_VERSION="0.38.0" curl -L https://github.com/DataBiosphere/terra-cli/releases/latest/download/download-install.sh | bash
-echo 'export PATH=${HOME}/bin:${PATH}' >> "/home/${NOTEBOOK_USER}/.bash_profile"
 ./terra
 
 # Set browser manual since that is always what we want for cloudtops.
 ./terra config set browser manual
+
+sudo cp terra /usr/bin/terra
+echo "copied"
 
 # TODO mount the current workspace?
