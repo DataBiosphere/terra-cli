3c09506a75f5d2ec8320b964a51718bdbb01d658
wchambers <1499870+wchamber@users.noreply.github.com>
2021-05-14 15:47:33 -0400

    tweaks

--- notebooks/post-startup.sh
+++ notebooks/post-startup.sh
@@ -20,23 +20,21 @@ VM_USER="jupyter"
 echo "/etc/passwd"
 cat /etc/passwd
 
-sudo -u ${VM_USR} cd /home/${VM_USER}
-
 # Install these globally (not in a virtual environment)\n",
-sudo -u ${VM_USER} pip3 install --upgrade pre-commit nbdime nbstripout pylint pytest
+sudo -u ${VM_USER} sh -c 'pip3 install --user --upgrade pre-commit nbdime nbstripout pylint pytest'
 
 # Install nbstripout globally
-sudo -u ${VM_USER} nbstripout --install
+sudo -u ${VM_USER} sh -c 'nbstripout --install'
 
 # Install & configure the Terra CLI
 
 # TODO should we just use the latest version?
 cd /tmp || exit
-env TERRA_CLI_VERSION="0.38.0" curl -L https://github.com/DataBiosphere/terra-cli/releases/latest/download/download-install.sh | sudo -u ${VM_USER} bash
-sudo -u ${VM_USER} ./terra
+sudo -u ${VM_USER} sh -c 'env TERRA_CLI_VERSION="0.38.0" curl -L https://github.com/DataBiosphere/terra-cli/releases/latest/download/download-install.sh | bash'
+sudo -u ${VM_USER} sh - c './terra'
 
 # Set browser manual since that is always what we want for cloudtops.
-sudo -u ${VM_USER} ./terra config set browser manual
+sudo -u ${VM_USER} sh -c './terra config set browser manual'
 
 sudo cp terra /usr/bin/terra
 echo "copied"
