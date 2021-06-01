88d363e2f2ce20f4d49cf5c25314ff92f9e5bf37
Mariko Medlock <mariko.medlock@gmail.com>
2021-06-01 12:42:11 -0400

    Re-enable nextflow run.

--- src/test/resources/testscripts/NextflowRnaseq.sh
+++ src/test/resources/testscripts/NextflowRnaseq.sh
@@ -67,7 +67,6 @@ terra nextflow config rnaseq-nf/main.nf -profile gls
 
 # And kick off the actual Nextflow workflow.
 
-# TODO: uncomment before merging
-#terra nextflow run rnaseq-nf/main.nf -profile gls
+terra nextflow run rnaseq-nf/main.nf -profile gls
 
 # This will take about 10 minutes to complete. [Switch tabs] I started this workflow in a different workspace earlier and here is the resulting HTML report [show local webpage].
