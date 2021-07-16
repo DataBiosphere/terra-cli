0e0e4bd5d7abd38f78456835a299aabd3d8eb580
Mariko Medlock <mariko.medlock@gmail.com>
2021-07-16 17:40:21 -0400

    Run full test suite + changes to docker/

--- docker/scripts/terra_init.sh
+++ docker/scripts/terra_init.sh
@@ -6,4 +6,6 @@ gcloud auth activate-service-account --key-file=${GOOGLE_APPLICATION_CREDENTIALS
 echo "Setting the gcloud project to the workspace project"
 gcloud config set project ${GOOGLE_CLOUD_PROJECT}
 
+echo "mariko test" > mariko.txt
+
 echo
