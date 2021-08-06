f38316e3781041f8fa23f7df345a5c5e9203dbea
John Czerkowicz <jczerk@verily.com>
2021-08-06 09:19:33 -0400

    Clean up formatting and try to make UTC implications clearer in help message.

--- src/main/java/bio/terra/cli/command/shared/options/BqDatasetLifetime.java
+++ src/main/java/bio/terra/cli/command/shared/options/BqDatasetLifetime.java
@@ -4,7 +4,7 @@ import picocli.CommandLine;
 
 /**
  * Command helper class that defines the default lifetime options for `terra resource` commands that
- * handle Big Query Datasets.
+ * handle controlled Big Query Datasets.
  *
  * <p>This class is meant to be used as a @CommandLine.Mixin.
  */
@@ -13,13 +13,25 @@ public class BqDatasetLifetime {
   @CommandLine.Option(
       names = "--default-partition-lifetime",
       description =
-          "The default lifetime, in seconds, for partitions in newly created partitioned tables. This flag has no minimum value. Specify 0 to remove the existing expiration time. Any partitions in newly created partitioned tables are deleted this many seconds after the partition's UTC date. This value is applied if you do not set a partition expiration on the table when it is created.")
+          "The default lifetime, in seconds, for partitions in newly created partitioned tables. "
+              + "This flag has no minimum value. Specify 0 to remove the existing expiration "
+              + "time. Any partitions in newly created partitioned tables are deleted this many "
+              + "seconds after the partition's create time, adjusted to UTC "
+              + "(@|underline,blue https://en.wikipedia.org/wiki/Coordinated_Universal_Time|@) "
+              + "such that time zone changes will not affect this lifetime.  This value is "
+              + "applied if you do not set a partition expiration on the table when it is "
+              + "created.")
   private Integer defaultPartitionLifetime;
 
   @CommandLine.Option(
       names = "--default-table-lifetime",
       description =
-          "The default lifetime, in seconds, for newly created tables. The minimum value is 3600 seconds (one hour). The expiration time evaluates to the current UTC time plus this value. Specify 0 to remove the existing expiration time. Any table created in the dataset is deleted this many seconds after its creation time. This value is applied if you do not set a table expiration when the table is created.")
+          "The default lifetime, in seconds, for newly created tables. The minimum value is "
+              + "3600 seconds (one hour). Specify 0 to remove the existing expiration time. The "
+              + "expiration time evaluates to the current time plus this value, adjusted to UTC "
+              + "such that time zone changes will not affect this lifetime. Any table created in "
+              + "the dataset is deleted this many seconds after its creation time. This value is "
+              + "applied if you do not set a table expiration when the table is created.")
   private Integer defaultTableLifetime;
 
   public boolean isDefined() {
