0882961df07324f7d85f5c7f4efb970bf111b4de
Mariko Medlock <mariko.medlock@gmail.com>
2021-09-15 10:57:03 -0400

    Check for 409 bad state in notebook start/stop.

--- src/main/java/bio/terra/cli/service/GoogleAiNotebooks.java
+++ src/main/java/bio/terra/cli/service/GoogleAiNotebooks.java
@@ -1,11 +1,13 @@
 package bio.terra.cli.service;
 
 import bio.terra.cli.exception.SystemException;
+import bio.terra.cli.exception.UserActionableException;
 import bio.terra.cli.service.utils.CrlUtils;
 import bio.terra.cloudres.google.api.services.common.OperationCow;
 import bio.terra.cloudres.google.api.services.common.OperationUtils;
 import bio.terra.cloudres.google.notebooks.AIPlatformNotebooksCow;
 import bio.terra.cloudres.google.notebooks.InstanceName;
+import com.google.api.client.googleapis.json.GoogleJsonResponseException;
 import com.google.api.services.notebooks.v1.model.Instance;
 import com.google.api.services.notebooks.v1.model.Operation;
 import com.google.auth.oauth2.GoogleCredentials;
@@ -32,6 +34,7 @@ public class GoogleAiNotebooks {
       Operation startOperation = notebooks.instances().start(instanceName).execute();
       pollForSuccess(startOperation, "Error starting notebook instance: ");
     } catch (InterruptedException | IOException e) {
+      checkFor409BadState(e);
       throw new SystemException("Error starting notebook instance", e);
     }
   }
@@ -41,6 +44,7 @@ public class GoogleAiNotebooks {
       Operation stopOperation = notebooks.instances().stop(instanceName).execute();
       pollForSuccess(stopOperation, "Error stopping notebook instance: ");
     } catch (InterruptedException | IOException e) {
+      checkFor409BadState(e);
       throw new SystemException("Error stopping notebook instance", e);
     }
   }
@@ -53,6 +57,25 @@ public class GoogleAiNotebooks {
             operationCow, Duration.ofSeconds(5), Duration.ofMinutes(3));
     if (operationCow.getOperation().getError() != null) {
       throw new SystemException(errorMessage + operationCow.getOperation().getError().getMessage());
+    }
+  }
+
+  /**
+   * If the exception is a 409 from GCP with "unable to queue the operation" message, then wrap in a
+   * UserActionableException and tell the user to try waiting a few minutes before trying again.
+   * There is an open issue against the java-notebooks library on GitHub for this:
+   * https://github.com/googleapis/java-notebooks/issues/201.
+   */
+  private void checkFor409BadState(Exception ex) {
+    if (ex instanceof GoogleJsonResponseException) {
+      GoogleJsonResponseException googleJsonEx = (GoogleJsonResponseException) ex;
+      int httpCode = googleJsonEx.getStatusCode();
+      String message = googleJsonEx.getDetails().getMessage();
+      if (httpCode == 409 && message.contains("unable to queue the operation")) {
+        throw new UserActionableException(
+            "Error starting notebook instance: The notebook is not in the right state to start/stop. Wait a few minutes and try again. (409: unable to queue the operation)",
+            googleJsonEx);
+      }
     }
   }
 }
