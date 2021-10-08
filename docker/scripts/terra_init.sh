03d6f60786221cd56a8c47b5e0f4e38163efcae3
Mariko Medlock <mariko.medlock@gmail.com>
2021-10-08 08:46:25 -0400

    Fix bad bash flag.

--- docker/scripts/terra_init.sh
+++ docker/scripts/terra_init.sh
@@ -3,7 +3,7 @@
 echo "Setting the gcloud project to the workspace project"
 gcloud config set project ${GOOGLE_CLOUD_PROJECT}
 
-if [ -z "$GOOGLE_APPLICATION_CREDENTIALS" ]; then
+if [ -n "$GOOGLE_APPLICATION_CREDENTIALS" ]; then
   echo "Setting the gcloud credentials to the user's pet service account"
   gcloud auth activate-service-account --key-file=${GOOGLE_APPLICATION_CREDENTIALS}
 fi
