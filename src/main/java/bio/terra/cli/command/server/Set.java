9e7ba8564f665b8ba5b56b411dfef663516812dd
Mariko Medlock <mariko.medlock@gmail.com>
2021-10-18 12:22:18 -0400

    Add confirmation prompt when switch server.

--- src/main/java/bio/terra/cli/command/server/Set.java
+++ src/main/java/bio/terra/cli/command/server/Set.java
@@ -3,6 +3,7 @@ package bio.terra.cli.command.server;
 import bio.terra.cli.businessobject.Context;
 import bio.terra.cli.businessobject.Server;
 import bio.terra.cli.command.shared.BaseCommand;
+import bio.terra.cli.command.shared.options.ConfirmationPrompt;
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
 
@@ -16,17 +17,37 @@ public class Set extends BaseCommand {
       description = "Server name. Run `terra server list` to see the available servers.")
   private String name;
 
+  @CommandLine.Mixin ConfirmationPrompt confirmationPromptOption;
+
   /** Update the Terra environment to which the CLI is pointing. */
   @Override
   protected void execute() {
     String prevServerName = Context.getServer().getName();
-    Context.setServer(Server.get(name));
+    Server newServer = Server.get(name);
+
+    boolean serverChanged = !prevServerName.equals(newServer.getName());
+    boolean loggedIn = Context.getUser().isPresent();
+    boolean workspaceSet = Context.getWorkspace().isPresent();
+    if (serverChanged && (loggedIn || workspaceSet)) {
+      confirmationPromptOption.confirmOrThrow(
+          "Switching the server will clear the current login credentials and workspace. Are you sure you want to proceed (y/N)?",
+          "Switching server aborted.");
+    }
+
+    // unset the current user and workspace
+    if (loggedIn) {
+      Context.requireUser().logout();
+    }
+    if (workspaceSet) {
+      Context.setWorkspace(null);
+    }
+    Context.setServer(newServer);
 
     OUT.println(
         "Terra server is set to "
             + Context.getServer().getName()
             + " ("
-            + (Context.getServer().getName().equals(prevServerName) ? "UNCHANGED" : "CHANGED")
+            + (serverChanged ? "CHANGED" : "UNCHANGED")
             + ").");
   }
 
--- src/main/java/bio/terra/cli/command/shared/options/DeletePrompt.java
+++ src/main/java/bio/terra/cli/command/shared/options/DeletePrompt.java
@@ -1,11 +1,6 @@
 package bio.terra.cli.command.shared.options;
 
 import bio.terra.cli.exception.UserActionableException;
-import bio.terra.cli.utils.UserIO;
-import java.io.PrintStream;
-import java.nio.charset.StandardCharsets;
-import java.util.Scanner;
-import picocli.CommandLine;
 
 /**
  * Command helper class that defines the --quiet flag for suppressing interactive user input, and
@@ -15,28 +10,13 @@ import picocli.CommandLine;
  *
  * <p>This class is meant to be used as a @CommandLine.Mixin.
  */
-public class DeletePrompt {
-
-  @CommandLine.Option(names = "--quiet", description = "Suppress interactive prompt and delete.")
-  private boolean quiet;
+public class DeletePrompt extends ConfirmationPrompt {
 
   /**
    * Helper method to generate an interactive confirmation prompt if the `--quiet` option is not
    * specified. Throws a {@link UserActionableException} if the prompt response is negative.
    */
   public void confirmOrThrow() {
-    boolean promptReturnedYes = true; // default to Y=yes if user input is suppressed
-    PrintStream OUT = UserIO.getOut();
-    if (!quiet) {
-      OUT.print("Are you sure you want to delete (y/N)? ");
-      Scanner reader = new Scanner(UserIO.getIn(), StandardCharsets.UTF_8);
-      String deletePrompt = reader.nextLine();
-      promptReturnedYes =
-          deletePrompt.equalsIgnoreCase("y") || deletePrompt.equalsIgnoreCase("yes");
-    }
-
-    if (!promptReturnedYes) {
-      throw new UserActionableException("Delete aborted.");
-    }
+    super.confirmOrThrow("Are you sure you want to delete (y/N)?", "Delete aborted.");
   }
 }
