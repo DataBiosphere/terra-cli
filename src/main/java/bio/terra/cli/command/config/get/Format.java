7642ca8dccbd009e8f495ba544bc8746e032853c
Jay Carlton <53479492+jaycarlton@users.noreply.github.com>
2021-11-17 15:00:36 -0500

    remove UFFormatConfig class

--- src/main/java/bio/terra/cli/command/config/get/Format.java
+++ src/main/java/bio/terra/cli/command/config/get/Format.java
@@ -2,7 +2,7 @@ package bio.terra.cli.command.config.get;
 
 import bio.terra.cli.businessobject.Context;
 import bio.terra.cli.command.shared.BaseCommand;
-import bio.terra.cli.serialization.userfacing.UFFormatConfig;
+import bio.terra.cli.command.shared.options.Format.FormatOptions;
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
 
@@ -13,13 +13,11 @@ public class Format extends BaseCommand {
 
   @Override
   protected void execute() {
-    UFFormatConfig formatOptionConfig =
-        new UFFormatConfig.Builder().formatOption(Context.getConfig().getFormat()).build();
-    formatOption.printReturnValue(formatOptionConfig, Format::printText);
+    formatOption.printReturnValue(Context.getConfig().getFormat(), Format::printText);
   }
 
-  public static void printText(UFFormatConfig returnValue) {
-    OUT.println("[text, json] output format = " + returnValue.format);
+  public static void printText(FormatOptions format) {
+    OUT.println("[text, json] output format = " + format);
   }
 
   /** This command never requires login. */
