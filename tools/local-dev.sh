fb836bf0b79c38c94958cd8752671a4c58c1ce62
Jay Carlton <53479492+jaycarlton@users.noreply.github.com>
2022-02-22 13:11:04 -0500

    [PF-1269] installation without Docker support (#191)

--- tools/local-dev.sh
+++ tools/local-dev.sh
@@ -6,7 +6,7 @@ set -e
 ## Usage: source tools/local-dev.sh
 
 ## The script assumes that it is being run from the top-level directory "terra-cli/".
-if [ $(basename $PWD) != 'terra-cli' ]; then
+if [ "$(basename "$PWD")" != 'terra-cli' ]; then
   echo "Script must be run from top-level directory 'terra-cli/'"
   exit 1
 fi
@@ -15,18 +15,18 @@ echo "Building Java code"
 ./gradlew clean install
 
 echo "Aliasing JAR file"
-alias terra=$(pwd)/build/install/terra-cli/bin/terra
+alias terra="$(pwd)"/build/install/terra-cli/bin/terra
 
 echo "Setting the Docker image id to the default"
 terra config set image --default
-
+terra config set app-launch DOCKER_CONTAINER
 echo "Pulling the default Docker image"
 defaultDockerImage=$(terra config get image)
-docker pull $defaultDockerImage
+docker pull "$defaultDockerImage"
 
 echo "Setting the server to its current value, to pull any changes"
 currentServer=$(terra config get server)
-terra config set server --name=$currentServer
+terra config set server --name="$currentServer"
 
 echo "Making all 'tools' scripts executable"
 chmod a+x tools/*
