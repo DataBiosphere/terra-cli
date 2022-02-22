b8add1de9a1a6806e950477757a5c0a7450727e3
Jay Carlton <53479492+jaycarlton@users.noreply.github.com>
2022-02-22 16:29:44 -0500

    [PF-1332] friendly error on missing executable (#205)

--- src/main/java/bio/terra/cli/command/app/passthrough/Nextflow.java
+++ src/main/java/bio/terra/cli/command/app/passthrough/Nextflow.java
@@ -1,25 +1,32 @@
 package bio.terra.cli.command.app.passthrough;
 
 import bio.terra.cli.businessobject.Context;
-import bio.terra.cli.command.shared.BaseCommand;
-import bio.terra.cli.command.shared.options.WorkspaceOverride;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-import picocli.CommandLine;
 import picocli.CommandLine.Command;
 
 /** This class corresponds to the second-level "terra nextflow" command. */
 @Command(name = "nextflow", description = "Call nextflow in the Terra workspace.")
-public class Nextflow extends BaseCommand {
+public class Nextflow extends ToolCommand {
 
-  @CommandLine.Mixin WorkspaceOverride workspaceOption;
-  @CommandLine.Unmatched private List<String> command = new ArrayList<>();
+  @Override
+  public String getExecutableName() {
+    return "nextflow";
+  }
+
+  @Override
+  public String getVersionArgument() {
+    return "-version";
+  }
+
+  @Override
+  public String getInstallationUrl() {
+    return "https://nextflow.io/";
+  }
 
   /** Pass the command through to the CLI Docker image. */
   @Override
-  protected void execute() {
+  protected void executeImpl() {
     workspaceOption.overrideIfSpecified();
     command.add(0, "nextflow");
     Map<String, String> envVars = new HashMap<>();
