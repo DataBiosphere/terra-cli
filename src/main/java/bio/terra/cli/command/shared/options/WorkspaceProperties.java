e62c992d268f4de2bd686a55cd010036fee92c33
Dexter Amundsen <dexamundsen@verily.com>
2022-08-17 11:25:25 -0700

    Apply format

--- src/main/java/bio/terra/cli/command/shared/options/WorkspaceProperties.java
+++ src/main/java/bio/terra/cli/command/shared/options/WorkspaceProperties.java
@@ -3,9 +3,7 @@ package bio.terra.cli.command.shared.options;
 import java.util.Map;
 import picocli.CommandLine;
 
-/**
- * @CommandLine.Mixin class for workspace properties options
- */
+/** @CommandLine.Mixin class for workspace properties options */
 public class WorkspaceProperties {
   @CommandLine.Option(
       names = "--properties",
