e07e726a5c595f3be5b99008cd3a6640377e43ce
Dexter Amundsen <dexamundsen@verily.com>
2022-10-12 11:42:59 -0700

    Tag messages as errors

--- tools/download-install.sh
+++ tools/download-install.sh
@@ -14,11 +14,11 @@ then
   readonly CUR_JAVA_VERSION="$(java -version 2>&1 | awk -F\" '{ split($2,a,"."); print a[1]}')"
   if [[ "${CUR_JAVA_VERSION}" -lt ${REQ_JAVA_VERSION} ]];
   then
-    echo "Java version set to ${CUR_JAVA_VERSION}, exiting..."
+    echo "Error with Java version: set to ${CUR_JAVA_VERSION}, exiting..."
     exit 1
   fi
 else
-  echo "Java installation not found, exiting"
+  echo "Error determining Java version, exiting"
   exit 1
 fi
 
