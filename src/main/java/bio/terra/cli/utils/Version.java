05da53d5e4db40fa665cc16f40a000183c8fb2ab
ginaygoogle <100877272+ginaygoogle@users.noreply.github.com>
2022-11-30 17:11:03 -0500

    fix the current version is null error

--- src/main/java/bio/terra/cli/utils/Version.java
+++ src/main/java/bio/terra/cli/utils/Version.java
@@ -5,6 +5,10 @@ public class Version {
   /** Getter for the Terra CLI version of the current JAR. */
   public static String getVersion() {
     // read from the JAR Manifest file
-    return Version.class.getPackage().getSpecificationVersion();
+    String version = Version.class.getPackage().getSpecificationVersion();
+    if (version != null) {
+      return version;
+    }
+    return System.getProperty("TERRA_VERSION");
   }
 }
