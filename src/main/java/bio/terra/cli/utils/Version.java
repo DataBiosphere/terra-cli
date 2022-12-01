9b4ec8333761d7c53f0a5b4368fafbc7a17c921d
ginaygoogle <100877272+ginaygoogle@users.noreply.github.com>
2022-12-01 12:04:16 -0500

    add the logger

--- src/main/java/bio/terra/cli/utils/Version.java
+++ src/main/java/bio/terra/cli/utils/Version.java
@@ -1,7 +1,10 @@
 package bio.terra.cli.utils;
 
+import org.slf4j.LoggerFactory;
+
 /** Utility methods for the currently installed version of the Terra CLI. */
 public class Version {
+  private static final org.slf4j.Logger logger = LoggerFactory.getLogger(Version.class);
   /** Getter for the Terra CLI version of the current JAR. */
   public static String getVersion() {
     // read from the JAR Manifest file
@@ -9,6 +12,8 @@ public class Version {
     if (version != null) {
       return version;
     }
+    logger.warn(
+        "Implementation version not defined in the JAR manifest. This is expected when testing, not during normal operation.");
     return System.getProperty("TERRA_CLI_VERSION");
   }
 }
