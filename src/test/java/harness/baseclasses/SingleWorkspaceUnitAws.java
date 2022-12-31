9de9cadc86681d22a786603a80d432eef9d94ad5
Dex Amundsen <dexamundsen@verily.com>
2022-12-30 20:08:46 -0800

    Rename

--- src/test/java/harness/baseclasses/SingleWorkspaceUnitAws.java
+++ src/test/java/harness/baseclasses/SingleWorkspaceUnitAws.java
@@ -4,7 +4,7 @@ import bio.terra.workspace.model.CloudPlatform;
 import java.util.Optional;
 
 /** Base class for AWS unit tests that only need a single workspace for all test methods. */
-public class AwsSingleWorkspaceUnit extends SingleWorkspaceUnit {
+public class SingleWorkspaceUnitAws extends SingleWorkspaceUnit {
   @Override
   protected Optional<CloudPlatform> getPlatform() {
     return Optional.of(CloudPlatform.AWS);
