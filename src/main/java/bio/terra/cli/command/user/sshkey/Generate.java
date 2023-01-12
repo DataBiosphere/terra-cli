2f19df7a446f98d2941ba7b6f186c48b77a5379d
Yu Hu <93544261+yuhuyoyo@users.noreply.github.com>
2023-01-12 12:27:01 -0500

    PF-2376: add a ssh-key add command  (#394)
    * ssh-key add
    
    * fix test
    
    * add document

--- src/main/java/bio/terra/cli/command/user/sshkey/Generate.java
+++ src/main/java/bio/terra/cli/command/user/sshkey/Generate.java
@@ -6,9 +6,6 @@ import bio.terra.cli.command.shared.options.Format;
 import bio.terra.cli.serialization.userfacing.UFSshKeyPair;
 import bio.terra.cli.service.ExternalCredentialsManagerService;
 import bio.terra.externalcreds.model.SshKeyPairType;
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
 
@@ -29,24 +26,13 @@ public class Generate extends BaseCommand {
     confirmationPrompt.confirmOrThrow(
         "Generating a new Terra SSH key will replace the old Terra SSH key if it exists. "
             + "You must associate the new SSH public key with your GitHub account using "
-            + "https://docs.github.com/en/authentication/connecting-to-github-with-ssh/generating-a-new-ssh-key-and-adding-it-to-the-ssh-agent#adding-your-ssh-key-to-the-ssh-agent. "
+            + "https://docs.github.com/en/authentication/connecting-to-github-with-ssh/adding-a-new-ssh-key-to-your-github-account "
             + "Are you sure you want to proceed (y/N)?",
         "Generating new SSH key is aborted");
-    var ecmService = ExternalCredentialsManagerService.fromContext();
-    var sshKeyPair = ecmService.generateSshKeyPair(SshKeyPairType.GITHUB);
+    var sshKeyPair =
+        ExternalCredentialsManagerService.fromContext().generateSshKeyPair(SshKeyPairType.GITHUB);
     if (saveToFile) {
-      try {
-        BufferedWriter privateWriter = new BufferedWriter(new FileWriter("terra_id_rsa"));
-        privateWriter.write(sshKeyPair.getPrivateKey());
-        privateWriter.close();
-        BufferedWriter publicWriter = new BufferedWriter(new FileWriter("terra_id_rsa.pub"));
-        publicWriter.write(sshKeyPair.getPublicKey());
-        publicWriter.close();
-        OUT.println(
-            "Ssh private key is saved in terra_id_rsa and Ssh public key is saved in terra_id_rsa.pub. You can move them under ~/.ssh/.");
-      } catch (IOException e) {
-        OUT.println("Failed to write to file");
-      }
+      Add.saveKeyFileAndSshAdd(sshKeyPair);
     } else {
       formatOption.printReturnValue(UFSshKeyPair.createUFSshKey(sshKeyPair), UFSshKeyPair::print);
     }
--- src/test/java/unit/SshKeyPair.java
+++ src/test/java/unit/SshKeyPair.java
@@ -3,12 +3,17 @@ package unit;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import bio.terra.cli.serialization.userfacing.UFSshKeyPair;
 import harness.TestCommand;
 import harness.TestUser;
 import harness.baseclasses.SingleWorkspaceUnit;
+import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import org.apache.commons.io.FileUtils;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
@@ -38,6 +43,16 @@ public class SshKeyPair extends SingleWorkspaceUnit {
   }
 
   @Test
+  void addSshKey() throws IOException {
+    testUser.login();
+    TestCommand.runCommandExpectSuccess("user", "ssh-key", "generate", "--quiet");
+
+    TestCommand.runCommandExpectSuccess("user", "ssh-key", "add");
+
+    assertSshKeyAdded();
+  }
+
+  @Test
   void generateSshKey() throws IOException {
     testUser.login();
     var sshkey =
@@ -55,6 +70,15 @@ public class SshKeyPair extends SingleWorkspaceUnit {
   }
 
   @Test
+  void generateSshKey_saveToFile() throws IOException {
+    testUser.login();
+
+    TestCommand.runCommandExpectSuccess("user", "ssh-key", "generate", "--quiet", "--save-to-file");
+
+    assertSshKeyAdded();
+  }
+
+  @Test
   void switchUser() throws IOException {
     testUser.login();
     var sshkey =
@@ -67,5 +91,16 @@ public class SshKeyPair extends SingleWorkspaceUnit {
         TestCommand.runAndParseCommandExpectSuccess(
             UFSshKeyPair.class, "user", "ssh-key", "generate", "--quiet");
     assertEquals(testUser2.email.toLowerCase(), sshkey2.userEmail.toLowerCase());
+  }
+
+  private void assertSshKeyAdded() {
+    String privateKey = System.getProperty("user.home") + "/.ssh/terra_id_rsa";
+    String publicKey = System.getProperty("user.home") + "/.ssh/terra_id_rsa.pub";
+    assertTrue(Files.exists(Paths.get(privateKey)));
+    assertTrue(Files.exists(Paths.get(publicKey)));
+
+    // clean up
+    FileUtils.deleteQuietly(new File(privateKey));
+    FileUtils.deleteQuietly(new File(publicKey));
   }
 }
