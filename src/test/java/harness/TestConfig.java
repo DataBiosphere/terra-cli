a0cdf4d40827bfc8cae84a0e4eda36c6b798d153
Dex Amundsen <dexamundsen@verily.com>
2023-01-25 16:57:15 -0800

    spotlessJavaCheck

--- src/test/java/harness/TestConfig.java
+++ src/test/java/harness/TestConfig.java
@@ -5,13 +5,12 @@ import bio.terra.cli.utils.FileUtils;
 import bio.terra.cli.utils.JacksonMapper;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.google.common.base.Preconditions;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /** Test config that can vary between Terra deployments. */
 public final class TestConfig {
