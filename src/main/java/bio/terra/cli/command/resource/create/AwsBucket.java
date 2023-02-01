549905a7f816ab3f7b9fc9dcb3927d5207e8ee9a
Dexter Amundsen <dexamundsen@verily.com>
2023-01-31 16:43:05 -0800

    rebase 1/31

---
+++ src/main/java/bio/terra/cli/command/resource/create/AwsBucket.java
@@ -0,0 +1,57 @@
+package bio.terra.cli.command.resource.create;
+
+import bio.terra.cli.command.shared.WsmBaseCommand;
+import bio.terra.cli.command.shared.options.ControlledResourceCreation;
+import bio.terra.cli.command.shared.options.Format;
+import bio.terra.cli.command.shared.options.WorkspaceOverride;
+import bio.terra.cli.serialization.userfacing.input.CreateAwsBucketParams;
+import bio.terra.cli.serialization.userfacing.input.CreateResourceParams;
+import bio.terra.cli.serialization.userfacing.resource.UFAwsBucket;
+import bio.terra.workspace.model.StewardshipType;
+import picocli.CommandLine;
+
+/** This class corresponds to the fourth-level "terra resource create aws-bucket" command. */
+@CommandLine.Command(
+    name = "aws-bucket",
+    description = "Add a controlled AWS bucket.",
+    showDefaultValues = true)
+public class AwsBucket extends WsmBaseCommand {
+  @CommandLine.Mixin ControlledResourceCreation controlledResourceCreationOptions;
+  @CommandLine.Mixin WorkspaceOverride workspaceOption;
+  @CommandLine.Mixin Format formatOption;
+
+  @CommandLine.Option(
+      names = "--location",
+      defaultValue = "us-east-1",
+      description =
+          "The AWS location of the bucket (https://docs.aws.amazon.com/general/latest/gr/s3.html).")
+  private String location;
+
+  /** Print this command's output in text format. */
+  private static void printText(UFAwsBucket returnValue) {
+    OUT.println("Successfully added controlled AWS bucket.");
+    returnValue.print();
+  }
+
+  /** Add a controlled AWS bucket to the workspace. */
+  @Override
+  protected void execute() {
+    workspaceOption.overrideIfSpecified();
+
+    // build the resource object to create
+    CreateResourceParams.Builder createResourceParams =
+        controlledResourceCreationOptions
+            .populateMetadataFields()
+            .stewardshipType(StewardshipType.CONTROLLED);
+    CreateAwsBucketParams.Builder createParams =
+        new CreateAwsBucketParams.Builder()
+            .resourceFields(createResourceParams.build())
+            .location(location);
+
+    bio.terra.cli.businessobject.resource.AwsBucket createdResource =
+        bio.terra.cli.businessobject.resource.AwsBucket.createControlled(createParams.build());
+    formatOption.printReturnValue(
+        new UFAwsBucket(createdResource),
+        bio.terra.cli.command.resource.create.AwsBucket::printText);
+  }
+}
