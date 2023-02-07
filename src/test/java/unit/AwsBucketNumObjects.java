35092c0a353714b3a662e6381cffdfabca86fd5d
Dex Amundsen <dexamundsen@verily.com>
2023-02-07 13:00:22 -0800

    backup

---
+++ src/test/java/unit/AwsBucketNumObjects.java
@@ -0,0 +1,11 @@
+package unit;
+
+import harness.baseclasses.SingleWorkspaceUnitAws;
+import org.junit.jupiter.api.Tag;
+
+/** Tests for including the number of objects in a AWS bucket resource's description. */
+@Tag("unit-aws")
+public class AwsBucketNumObjects extends SingleWorkspaceUnitAws {
+  // todo-dex
+
+}
