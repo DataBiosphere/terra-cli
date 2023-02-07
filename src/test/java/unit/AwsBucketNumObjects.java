2c9404a7b80e740bfa2ecc894da6fe334f33b13a
Dex Amundsen <dexamundsen@verily.com>
2023-02-06 17:13:14 -0800

    cleanup

--- src/test/java/unit/AwsBucketNumObjects.java
+++
@@ -1,8 +0,0 @@
-package unit;
-
-import harness.baseclasses.SingleWorkspaceUnitAws;
-import org.junit.jupiter.api.Tag;
-
-/** Tests for the `terra resource` commands that handle controlled AWS buckets. */
-@Tag("unit-aws")
-public class AwsBucketNumObjects extends SingleWorkspaceUnitAws {}
