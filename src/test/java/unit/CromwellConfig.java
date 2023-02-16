35e26755b8ed9fc782e9b722d78d52cee56d97b6
Willy Nojopranoto <nojopranoto@gmail.com>
2023-02-16 21:35:50 +0000

    Add test that checks for appropriate errors

--- src/test/java/unit/CromwellConfig.java
+++ src/test/java/unit/CromwellConfig.java
@@ -1,12 +1,13 @@
 package unit;
 
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import harness.TestCommand;
 import harness.baseclasses.SingleWorkspaceUnit;
 import java.io.File;
 import java.io.IOException;
-import org.junit.jupiter.api.Disabled;
+import org.hamcrest.CoreMatchers;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -23,8 +24,8 @@ public class CromwellConfig extends SingleWorkspaceUnit {
     // `terra workspace set --id=$id`
     TestCommand.runCommandExpectSuccess("workspace", "set", "--id=" + getUserFacingId());
 
-    // `terra cromwell generate-config`
-    TestCommand.runCommandExpectSuccess("cromwell", "generate-config");
+    // `terra cromwell generate-config --google-bucket-name=foo`
+    TestCommand.runCommandExpectSuccess("cromwell", "generate-config", "--google-bucket-name=foo");
 
     // New cromwell.conf file generate successfully.
     assertTrue(new File("cromwell.conf").isFile());
@@ -41,7 +42,7 @@ public class CromwellConfig extends SingleWorkspaceUnit {
     // `terra workspace set --id=$id`
     TestCommand.runCommandExpectSuccess("workspace", "set", "--id=" + getUserFacingId());
 
-    // `terra cromwell generate-config --dir=build --google-bucket-name`
+    // `terra cromwell generate-config --dir=build --google-bucket-name=foo`
     TestCommand.runCommandExpectSuccess(
         "cromwell", "generate-config", "--dir=build", "--google-bucket-name=foo");
 
@@ -51,5 +52,34 @@ public class CromwellConfig extends SingleWorkspaceUnit {
 
     // Remove the created config file.
     new File("./build/cromwell.conf").delete();
+  }
+
+  @Test
+  @DisplayName("cromwell generate-config check errors with bucket params")
+  void cromwellGenerateConfigBucketParamErrors() throws IOException {
+    workspaceCreator.login();
+
+    // `terra workspace set --id=$id`
+    TestCommand.runCommandExpectSuccess("workspace", "set", "--id=" + getUserFacingId());
+
+    // `terra cromwell generate-config`
+    String stdErr = TestCommand.runCommandExpectExitCode(2, "cromwell", "generate-config");
+    assertThat(
+        "error message indicates missing required argument",
+        stdErr,
+        CoreMatchers.containsString("Missing required argument (specify one of these):"));
+
+    // `terra cromwell generate-config --workspace-bucket-name=foo --google-bucket-name=foo2`
+    stdErr =
+        TestCommand.runCommandExpectExitCode(
+            2,
+            "cromwell",
+            "generate-config",
+            "--workspace-bucket-name=foo",
+            "--google-bucket-name=foo2");
+    assertThat(
+        "error message indicates only one must be specified",
+        stdErr,
+        CoreMatchers.containsString("are mutually exclusive (specify only one)"));
   }
 }
