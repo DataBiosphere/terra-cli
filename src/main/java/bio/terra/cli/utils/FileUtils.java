cdcbb7046747f6d28e85934480e74b259f1e98ca
Yu Hu <yuhuyoyo@google.com>
2023-02-23 12:28:38 -0500

    lint

--- src/main/java/bio/terra/cli/utils/FileUtils.java
+++ src/main/java/bio/terra/cli/utils/FileUtils.java
@@ -9,7 +9,6 @@ import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.Optional;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
