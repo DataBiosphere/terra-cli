d853ad5e04455e026e916c36203425ec36b7168d
Dex Amundsen <dexamundsen@verily.com>
2023-02-24 16:54:27 -0800

    SpotlessApply

--- src/main/java/bio/terra/cli/businessobject/Resource.java
+++ src/main/java/bio/terra/cli/businessobject/Resource.java
@@ -2,7 +2,12 @@ package bio.terra.cli.businessobject;
 
 import bio.terra.cli.businessobject.resource.AwsBucket;
 import bio.terra.cli.businessobject.resource.AwsNotebook;
-import bio.terra.cli.businessobject.resource.*;
+import bio.terra.cli.businessobject.resource.BqDataset;
+import bio.terra.cli.businessobject.resource.BqTable;
+import bio.terra.cli.businessobject.resource.GcpNotebook;
+import bio.terra.cli.businessobject.resource.GcsBucket;
+import bio.terra.cli.businessobject.resource.GcsObject;
+import bio.terra.cli.businessobject.resource.GitRepo;
 import bio.terra.cli.exception.UserActionableException;
 import bio.terra.cli.serialization.persisted.PDResource;
 import bio.terra.cli.serialization.userfacing.UFResource;
