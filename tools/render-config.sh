c78f8d589101be5e38e097d47ccb41036e3513be
Yu Hu <yuhuyoyo@google.com>
2023-06-22 10:40:35 -0400

    enable auth0 up to preprod

--- tools/render-config.sh
+++ tools/render-config.sh
@@ -106,3 +106,13 @@ clientSecret=$(docker run --rm -e VAULT_TOKEN="${VAULT_TOKEN}" "${DSDE_TOOLBOX_D
             jq -r '.data."verily-auth0-dev-client-secret"')
 ./tools/client-credentials.sh "src/main/resources/verily_auth0_dev_secret.json" "${clientId}" "${clientSecret}" \
                               "rendered/verily_auth0_dev_secret.json"
+
+echo "Fetching Verily auth0 prod client id and client secrets"
+clientId=$(docker run --rm -e VAULT_TOKEN="${VAULT_TOKEN}" "${DSDE_TOOLBOX_DOCKER_IMAGE}" \
+            vault read -format json "${CLIENT_CRED_VAULT_PATH}" | \
+            jq -r '.data."verily-auth0-prod-client-id"')
+clientSecret=$(docker run --rm -e VAULT_TOKEN="${VAULT_TOKEN}" "${DSDE_TOOLBOX_DOCKER_IMAGE}" \
+            vault read -format json "${CLIENT_CRED_VAULT_PATH}" | \
+            jq -r '.data."verily-auth0-prod-client-secret"')
+./tools/client-credentials.sh "src/main/resources/verily_auth0_secret.json" "${clientId}" "${clientSecret}" \
+                              "rendered/verily_auth0_secret.json"
